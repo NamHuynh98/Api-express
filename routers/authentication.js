42627aca6e3229a435262ba68f7500e9f7b661c8
huynh nam <namquanghuynh1998@gmail.com>
2021-04-13 22:30:14 +0700

    format link api

--- routers/authentication.js
+++ routers/authentication.js
@@ -32,7 +32,7 @@ module.exports = {
                 .catch(err => console.log(error))
         }
 
-        app.post("/token", (req, res) => {
+        app.post(`${process.env.ROUTER_ENV}/token`, (req, res) => {
             const refreshToken = req.body.refreshToken;
             if (!refreshToken) return res.sendStatus(401);
 
@@ -50,10 +50,10 @@ module.exports = {
                 })
         })
 
-        app.post("/login", (req, res) => {
+        app.post(`${process.env.ROUTER_ENV}/login`, (req, res) => {
             Auth.find({ username: req.body.username, password: req.body.password })
                 .then((data) => {
-                    if (data.length === 0) return res.status(401);
+                    if (data.length === 0) return res.sendStatus(401);
                     const tokens = generateTokens({
                         username: data[0].username,
                         id: data[0]._id,
@@ -66,20 +66,20 @@ module.exports = {
                 })
                 .catch((error) => {
                     console.log(error);
-                    return res.status(403);
+                    return res.sendStatus(403);
                 });
         })
 
-        app.delete("/logout", verifyToken, (req, res) => {
+        app.delete(`${process.env.ROUTER_ENV}/logout`, verifyToken, (req, res) => {
             Auth.find({ id: req.body.username })
                 .then((data) => {
-                    if (data.length === 0) return res.status(401);
+                    if (data.length === 0) return res.sendStatus(401);
                     updateRefreshToken({ username: data[0].username, id: data[0]._id }, null);
                     res.sendStatus(204);
                 })
                 .catch((error) => {
                     console.log(error);
-                    return res.status(403);
+                    return res.sendStatus(403);
                 });
         })
     }
